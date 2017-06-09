0ef5e5bb916d9ab83f38264cc32f352b344dbd02
Gabriel Schubert <gabrielschubert93@gmail.com>
2017-06-08 23:47:53 -0300

    No changes...

--- Tests/Test_pyqt_matplotlib.py
+++ Tests/Test_pyqt_matplotlib.py
@@ -1,13 +1,14 @@
 import sys
- 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QPushButton
 from PyQt5.QtGui import QIcon
- 
- 
+#******************************************************************
+import matplotlib
+matplotlib.use("Qt5Agg")
+#******************************************************************
+from PyQt5 import QtCore 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
- 
 import random
  
 class App(QMainWindow):
